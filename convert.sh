d96e6aa2352a2f87bf36e40589ec8c4cfa059378
Martin Chan <mchan.philly@gmail.com>
2023-09-18 21:28:47 -0400

    Make it easier to build (just Ctrl Enter)

--- convert.sh
+++ convert.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-npx js-yaml syntaxes/bluespec.tmLanguage.yaml > syntaxes/bluespec.tmLanguage.json
+npx js-yaml syntaxes/bluespec.tmLanguage.yaml > syntaxes/bluespec.tmLanguage.json &  # & is to make the window exit quietly
